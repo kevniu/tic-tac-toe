b4cae40e365fe69714053c04d1985cca784bb835
Kevin Niu <kev.niu@gmail.com>
2016-06-19 01:27:10 -0700

    Update js file

--- application.js
+++ application.js
@@ -4,18 +4,11 @@ $(document).ready(function() {
     var col1Counter = 1
     var col2Counter = 1
     var col3Counter = 1
-    var col4Counter = 1
-    var col5Counter = 1
-    var col6Counter = 1
-    var col7Counter = 1
 
     var board = [
-        ["0", "0", "0", "0", "0", "0", "0"],
-        ["0", "0", "0", "0", "0", "0", "0"],
-        ["0", "0", "0", "0", "0", "0", "0"],
-        ["0", "0", "0", "0", "0", "0", "0"],
-        ["0", "0", "0", "0", "0", "0", "0"],
-        ["0", "0", "0", "0", "0", "0", "0"]
+        ["0", "0", "0"],
+        ["0", "0", "0"],
+        ["0", "0", "0"],
     ]
 
     function isOdd(num) {
@@ -32,10 +25,10 @@ $(document).ready(function() {
             board[row - 2][col] == board[row - 3][col]) {
             win = true
             if (win == true && isOdd(playerTurn) == false) {
-                alert("Red Wins!")
+                alert("X Wins!")
                 location.reload();
             } else if (win == true && isOdd(playerTurn) == true) {
-                alert("Black Wins!")
+                alert("O Wins!")
                 location.reload();
             }
         }
@@ -44,13 +37,13 @@ $(document).ready(function() {
     function horizontalCheck(board, rowNum) {
         var row = board[rowNum]
         var rowContent = row.join('')
-        if (rowContent.includes("RRRR") == true) {
+        if (rowContent.includes("XXX") == true) {
             win = true
-            alert("Red Wins!")
+            alert("X Wins!")
             location.reload();
-        } else if (rowContent.includes("BBBB") == true) {
+        } else if (rowContent.includes("OOO") == true) {
             win = true
-            alert("Black Wins!")
+            alert("O Wins!")
             location.reload();
         }
     }
@@ -65,13 +58,13 @@ $(document).ready(function() {
             } catch (e) {}
         }
         var diagonalContent = diagonal.join('')
-        if (diagonalContent.includes("RRRR") == true) {
+        if (diagonalContent.includes("XXX") == true) {
             win = true
-            alert("Red Wins!")
+            alert("X Wins!")
             location.reload();
-        } else if (diagonalContent.includes("BBBB") == true) {
+        } else if (diagonalContent.includes("OOO") == true) {
             win = true
-            alert("Black Wins!")
+            alert("O Wins!")
             location.reload();
         }
     }
@@ -86,13 +79,13 @@ $(document).ready(function() {
             } catch (e) {}
         }
         var invDiagonalContent = invDiagonal.join('')
-        if (invDiagonalContent.includes("RRRR") == true) {
+        if (invDiagonalContent.includes("XXX") == true) {
             win = true
-            alert("Red Wins!")
+            alert("X Wins!")
             location.reload();
-        } else if (invDiagonalContent.includes("BBBB") == true) {
+        } else if (invDiagonalContent.includes("OOO") == true) {
             win = true
-            alert("Black Wins!")
+            alert("O Wins!")
             location.reload();
         }
     }
@@ -104,115 +97,52 @@ $(document).ready(function() {
       verticalCheck(board, col, row)
     }
 
-    $('#col1.space').click(function(e) {
+    $('#col1.square').click(function(e) {
         e.preventDefault();
         playerTurn += 1
         if (isOdd(playerTurn) == true) {
             $('#row' + col1Counter + ' #col1').addClass('black');
             col1Counter += 1
-            board[(col1Counter - 2)][0] = "B"
+            board[(col1Counter - 2)][0] = "X"
             checkWin(board, 0, (col1Counter - 2))
         } else if (isOdd(playerTurn) == false) {
             $('#row' + col1Counter + ' #col1').addClass('red');
             col1Counter += 1
-            board[(col1Counter - 2)][0] = "R"
+            board[(col1Counter - 2)][0] = "O"
             checkWin(board, 0, (col1Counter - 2))
         }
     })
 
-    $('#col2.space').click(function(e) {
+    $('#col2.square').click(function(e) {
         e.preventDefault();
         playerTurn += 1
         if (isOdd(playerTurn) == true) {
             $('#row' + col2Counter + ' #col2').addClass('black');
             col2Counter += 1
-            board[(col2Counter - 2)][1] = "B"
+            board[(col2Counter - 2)][1] = "X"
             checkWin(board, 1, (col2Counter - 2))
         } else if (isOdd(playerTurn) == false) {
             $('#row' + column2_counter + ' #col2').addClass('red');
             col2Counter += 1
-            board[(col2Counter - 2)][1] = "R"
+            board[(col2Counter - 2)][1] = "O"
             checkWin(board, 1, (col2Counter - 2))
         }
     })
 
-    $('#col3.space').click(function(e) {
+    $('#col3.squares').click(function(e) {
         e.preventDefault();
         playerTurn += 1
         if (isOdd(playerTurn) == true) {
             $('#row' + col3Counter + ' #col3').addClass('black');
             col3Counter += 1
-            board[(col3Counter - 2)][2] = "B"
+            board[(col3Counter - 2)][2] = "X"
             checkWin(board, 2, (col3Counter - 2))
         } else if (isOdd(playerTurn) == false) {
             $('#row' + col3Counter + ' #col3').addClass('red');
             col3Counter += 1
-            board[(col3Counter - 2)][2] = "R"
+            board[(col3Counter - 2)][2] = "O"
             checkWin(board, 2, (col3Counter - 2))
         }
     })
 
-    $('#col4.space').click(function(e) {
-        e.preventDefault();
-        playerTurn += 1
-        if (isOdd(playerTurn) == true) {
-            $('#row' + col4Counter + ' #col4').addClass('black');
-            col4Counter += 1
-            board[(col4Counter - 2)][3] = "B"
-            checkWin(board, 3, (col4Counter - 2))
-        } else if (isOdd(playerTurn) == false) {
-            $('#row' + col4Counter + ' #col4').addClass('red');
-            col4Counter += 1
-            board[(col4Counter - 2)][3] = "R"
-            checkWin(board, 3, (col4Counter - 2))
-        }
-    })
-
-    $('#col5.space').click(function(e) {
-        e.preventDefault();
-        playerTurn += 1
-        if (isOdd(playerTurn) == true) {
-            $('#row' + col5Counter + ' #col5').addClass('black');
-            col5Counter += 1
-            board[(col5Counter - 2)][4] = "B"
-            checkWin(board, 4, (col5Counter - 2))
-        } else if (isOdd(playerTurn) == false) {
-            $('#row' + col5Counter + ' #col5').addClass('red');
-            col5Counter += 1
-            board[(col5Counter - 2)][4] = "R"
-            checkWin(board, 4, (col5Counter - 2))
-        }
-    })
-
-    $('#col6.space').click(function(e) {
-        e.preventDefault();
-        playerTurn += 1
-        if (isOdd(playerTurn) == true) {
-            $('#row' + col6Counter + ' #col6').addClass('black');
-            col6Counter += 1
-            board[(col6Counter - 2)][5] = "B"
-            checkWin(board, 5, (col6Counter - 2))
-        } else if (isOdd(playerTurn) == false) {
-            $('#row' + col6Counter + ' #col6').addClass('red');
-            col6Counter += 1
-            board[(col6Counter - 2)][5] = "R"
-            checkWin(board, 5, (col6Counter - 2))
-        }
-    })
-
-    $('#col7.space').click(function(e) {
-        e.preventDefault();
-        playerTurn += 1
-        if (isOdd(playerTurn) == true) {
-            $('#row' + col7Counter + ' #col7').addClass('black');
-            col7Counter += 1
-            board[(col7Counter - 2)][6] = "B"
-            checkWin(board, 6, (col7Counter - 2))
-        } else if (isOdd(playerTurn) == false) {
-            $('#row' + col7Counter + ' #col7').addClass('red');
-            col7Counter += 1
-            board[(col7Counter - 2)][6] = "R"
-            checkWin(board, 6, (col7Counter - 2))
-        }
-    })
 });
